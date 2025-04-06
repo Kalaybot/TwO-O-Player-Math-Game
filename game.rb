855bda062d0e858203019fb308b6f5c6b2b58b00
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-06 16:13:18 -0400

    Remove all debugging statements and testing library, game works

--- game.rb
+++ game.rb
@@ -2,8 +2,6 @@ require_relative 'player'
 require_relative 'question'
 require_relative 'prompt'
 
-require 'byebug'
-
 class Game
   # Reader for players and prompt
   attr_reader :player1, :player2
@@ -13,8 +11,6 @@ class Game
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
     @cur_player = @player1 # Set the current player to player 1
-
-    byebug
   end
 
   # Start the game loop
@@ -22,16 +18,12 @@ class Game
     # Loop until one player runs out of lives
     while @player1.alive? && @player2.alive?
 
-      puts "Current player: #{@cur_player.name}, Lives: #{@cur_player.lives}"
-
       # Create a new question
       question = Question.new
 
       # Ask the current player the question
       Prompt.display(question.ask_question(@cur_player))
       answer = Prompt.get_input("Your answer: ")
-
-      puts "Received answer: #{answer}"
 
       # Check if the answer is correct
       if question.correct_answer?(answer)
@@ -43,8 +35,6 @@ class Game
       
       # Display the current lives of both players every turn
       Prompt.show_lives(@player1, @player2)
-
-      raise "Lives mismatch!" unless @player1.lives + @player2.lives == 6
 
       # Switch to the other player
       @cur_player = @cur_player == @player1 ? @player2 : @player1
